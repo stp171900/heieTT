62e8dd1e9c47f0001d0b4400c6bc880a70d18c5e
stp171900 <1412893745@qq.com>
2019-07-27 15:50:15 +0800

    heimTT项目初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/router'
+import store from './store/store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
